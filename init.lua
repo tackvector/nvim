10ff3744b6bf192ad5a28ebdbd1ac4caa363d08e
dorrajmachai <dwimble22@protonmail.com>
2023-03-18 10:06:05 -0600

    adding notes

--- init.lua
+++ init.lua
@@ -35,6 +35,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- NOTE: This is in the wrong place but I don't want to keep fighting with it, tbh (lowercase global error).
 opts = {
 	ui = {
 		border = "rounded"
