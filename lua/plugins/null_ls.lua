dc91bd68fc6001efca43c6070336485494ffe315
dorrajmachai <dwimble22@protonmail.com>
2023-03-18 06:22:17 -0600

    still null-ls

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -8,23 +8,12 @@ return {
 	config = function()
 		local null_ls = require('null-ls')
 
-		-- code action sources
-		local code_actions = null_ls.builtins.code_actions
-
-		-- diagnostic sources
-		local diagnostics = null_ls.builtins.diagnostics
-
-		-- formatting sources
-		local formatting = null_ls.builtins.formatting
-
-		-- hover sources
-		local hover = null_ls.builtins.hover
-
-		-- completion sources
-		local completion = null_ls.builtins.completion
-
-		-- spelling sources
-		local spell = null_ls.builtins.completion.spell
+		local code_actions = null_ls.builtins.code_actions -- code actions sources
+		local diagnostics = null_ls.builtins.diagnostics -- diagnostics sources
+		local formatting = null_ls.builtins.formatting -- formatting sources 
+		local hover = null_ls.builtins.hover -- hover sources 
+		local completion = null_ls.builtins.completion -- completion sources
+		local spell = null_ls.builtins.completion.spell -- spelling sources
 
 		local sources = {
 			code_actions.eslint_d,
