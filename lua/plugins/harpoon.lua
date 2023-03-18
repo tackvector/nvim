32b205a0bbb507f85888b447a7bbd042dd37a674
dorrajmachai <dwimble22@protonmail.com>
2023-03-18 16:19:45 -0600

    stable config

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -13,7 +13,7 @@ return {
 
 			vim.keymap.set("n", "<C-o>", function() ui.nav_file(1) end),
 			vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end),
-			vim.keymap.set("n", "<C-h>", function() ui.nav_file(3) end),
+			vim.keymap.set("n", "<C-i>", function() ui.nav_file(3) end),
 			vim.keymap.set("n", "<C-f>", function() ui.nav_file(4) end),
 		}
 	end
