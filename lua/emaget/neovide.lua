112c7a82655740721bddb05c70804e38299db07f
dorrajmachai <dwimble22@protonmail.com>
2023-03-25 17:07:20 -0600

    keybindings still broken, adding neovide stuff

--- lua/emaget/neovide.lua
+++ lua/emaget/neovide.lua
@@ -3,6 +3,10 @@
 --------------------------
 
 if vim.g.neovide then
-	vim.o.guifont = "Iosevka Nerd Font:h16"
+	vim.o.guifont = "Iosevka Nerd Font:h14"
 	vim.opt.linespace = 1
+	vim.g.neovide_hide_mouse_when_typing = true
+	vim.g.neovide_fullscreen = true
+	vim.g.neovide_cursor_animation_length = 0.01
+	vim.g.neovide_cursor_trail_size = 0.5
 end
