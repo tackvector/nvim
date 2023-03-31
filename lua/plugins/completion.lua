bdc6f04a04925bc8a0721079139dee0ae7580300
dorrajmachai <dwimble22@protonmail.com>
2023-03-31 11:39:23 -0600

    working on cmp

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -12,7 +12,27 @@ return {
 	-- snippets with LuaSnip
 	{ 'L3MON4D3/LuaSnip' },
 	config = function()
+		local luasnip = require('luasnip')
 		local cmp = require('cmp')
-
+		cmp.setup {
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end
+			},
+			mapping = {
+				["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+				["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+				["<C-C>"] = cmp.mapping {
+					i = cmp.mapping.abort(),
+					c = cmp.mapping.close(),
+				},
+				["<C-y>"] = cmp.mapping.confirm {
+					behavior = cmp.ConfirmBehavior.Insert,
+					select = true,
+				},
+				["<C-space>"] = cmp.mapping.complete(),
+			},
+		}
 	end
 }
