b2e8149ea75dad92e6a56879ebcd6395a66c4000
dorrajmachai <dwimble22@protonmail.com>
2023-05-24 02:07:04 -0600

    changed separator appearance

--- lua/plugins/moonfly_colors.lua
+++ lua/plugins/moonfly_colors.lua
@@ -6,4 +6,8 @@ return {
 	'bluz71/vim-moonfly-colors', name = 'moonfly',
 	lazy = true,
 	priority = 1000,
+	config = function ()
+		vim.g.moonflyWinSeparator = 2 -- display line separator
+		vim.opt.fillchars = { horiz = '━', horizup = '┻', horizdown = '┳', vert = '┃', vertleft = '┫', vertright = '┣', verthoriz = '╋', } -- improve the look of the separators
+	end
 }
