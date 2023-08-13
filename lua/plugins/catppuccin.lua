8540d43f382c88c16a6c4d96af428154ad49e4c9
dorrajmachai <dwimble22@protonmail.com>
2023-08-12 23:39:55 -0600

    removed rm_testing

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,35 +3,41 @@
 --------------------------
 
 return {
-	'catppuccin/nvim', name = 'catppuccin',
-	config = function()
-		require('catppuccin').setup {
-			color_overrides = {
-				-- voidppuccin overrides
-				mocha = {
---					base = "#000000",
---					mantle = "#000000",
---					crust = "#000000",
-				},
-			},
-			highlight_overrides = {
-				mocha = function(C)
-					return {
-						MiniTabLineFill = { bg = C.mantle },
-						MiniTabLineVisible = { bg = C.mantle, fg = C.overlay1 },
-						MiniTabLineHidden = { bg = C.mantle, fg = C.overlay1 },
-						MiniTabLineCurrent = { bg = C.surface0, fg = C.green },
-						MiniTabLineModifiedCurrent = { bg = C.surface0, fg = C.yellow },
-						MiniTabLineModifiedHidden = { fg = C.red },
-						MiniTabLineModifiedVisible = { fg = C.yellow },
-						CmpBorder = { fg = C.surface2 },
-						Pmenu = { bg = C.none },
-						TelescopeBorder = { link = "FloatBorder" },
-						VertSplit = { fg = "#313244" },
-						LineNr = { fg = "#6c7086" },
-					}
-				end,
-			},
-		}
-	end
+    'catppuccin/nvim', name = 'catppuccin',
+    config = function()
+        require('catppuccin').setup {
+            color_overrides = {
+                mocha = {
+                    --------------------------
+                    -- voidppuccin overrides
+                    --------------------------
+                    base = "#080808",
+                    mantle = "#313244",
+                    -- crust = "#000000",
+                    --------------------------
+                    -- other overrides
+                    --------------------------
+                },
+            },
+            highlight_overrides = {
+                mocha = function(C)
+                    return {
+                        MiniTabLineFill = { bg = C.base },
+                        MiniTabLineVisible = { bg = C.mantle, fg = C.base },
+                        MiniTabLineHidden = { bg = C.mantle, fg = C.base },
+                        MiniTabLineCurrent = { bg = C.surface0, fg = C.green },
+                        MiniTabLineModifiedCurrent = { bg = C.surface0, fg = C.yellow },
+                        MiniTabLineModifiedHidden = { fg = C.red },
+                        MiniTabLineModifiedVisible = { fg = C.yellow },
+                        CmpBorder = { fg = C.surface2 },
+                        Pmenu = { bg = C.none },
+                        TelescopeBorder = { link = "FloatBorder" },
+                        VertSplit = { fg = C.base },
+                        LineNr = { fg = "#6c7086" },
+                        SpellBad = { sp = C.red, style = { "undercurl" } },
+                    }
+                end,
+            },
+        }
+    end
 }
