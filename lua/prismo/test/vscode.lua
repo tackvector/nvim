da03332f9eb21e84925a5c1d543f50bea6754151
dorrajmachai <dwimble22@gmail.com>
2024-02-13 20:01:48 -0700

    added metals

--- lua/prismo/test/vscode.lua
+++ lua/prismo/test/vscode.lua
@@ -12,6 +12,7 @@ return {
                     -- this supports the same val table as vim.api.nvim_set_hl
                     -- use colors from this colorscheme by requiring vscode.colors!
                     Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
+                    NormalFloat = { bg = c.none },
                 },
             })
             vscode.load()
