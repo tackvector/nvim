f7e767704f0e6d010dc7ad2b8a2308d9d99c09c7
tackvector <dwimble22@gmail.com>
2024-06-23 19:58:19 -0600

    cleaned up init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
--- {{{ keybindings
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
@@ -34,48 +33,44 @@ for _, mapping in pairs(mappings) do
     local mode, lhs, rhs, opts = unpack(mapping)
     vim.keymap.set(mode, lhs, rhs, opts)
 end
--- }}}
 
 -- open help files in a vertical split
 vim.api.nvim_create_autocmd('FileType', { pattern = 'help', command = ':wincmd L' })
 
------------------------------
---[[ Options ]]
------------------------------
 local options = {
-    backup = false, -- no backups
-    clipboard = 'unnamedplus', -- always use clipboard for all operations
-    ch = 1, -- set height of command-line to 1
-    cole = 0, -- show text normally
-    fenc = 'utf-8', -- file encoding for current buffer
-    hls = false, -- don't highlight matches
-    is = true, -- show the pattern matches as i type
-    ph = 5, -- set the popup menu height to 10
-    tgc = true, -- enables 24-bit color in tui
-    sb = true, -- open new horizontal splits below current buffer
-    spr = true, -- open new vertical splits to the right of current buffer
-    swf = false, -- don't create a swapfile
-    tm = 1000, -- length of timeout for commands like 'jk' for exit
-    cindent = true, -- for C files, which I do not write right now.
-    nuw = 4, -- the width of the number column, setting it to 4 keeps it from pushing the editor over as numbers get larger
-    scl = 'yes', -- show signs in signcolumn
-    wrap = false, -- don't wrap lines, makes it easier for me to take notes
-    ls = 3, -- global statusline, thanks Neovim team :-)
-    et = true, -- changing things so they make sense to me
-    ts = 4, -- 4 spaces.
-    sts = 4,
-    sw = 4,
-    sr = true, -- found out about this from VimTricks. You should check it out if you haven't already! https://vim.town/@vimtricks
-    gcr = "n-v-c-i:block", -- i'll block you
-    sxq = "", -- i can use this to do cool things
-    ai = true, -- this should absolutely always be on
-    si = true, -- so should this.
-    so = 5, -- like sidescrolloff but for the bottom side
-    ea = true, -- all windows are made the same size after opening or closing
-    mouse = "", -- i believe in mirac-- *cue broom*
-    nu = true, -- yeah line numbers are nice...
-    rnu = true, -- ...especially when they're relative to the cursor position
-    fcs = { vert = ' ', },
+    backup = false,
+    clipboard = 'unnamedplus',
+    cmdheight = 1,
+    conceallevel = 0,
+    fileencoding = 'utf-8',
+    hlsearch = false,
+    incsearch = true,
+    pumheight = 5,
+    termguicolors = true,
+    splitbelow = true,
+    splitright = true,
+    swapfile = false,
+    timeoutlen = 1000,
+    cindent = true,
+    numberwidth = 4,
+    signcolumn = 'yes',
+    wrap = false,
+    laststatus = 3,
+    expandtab = true,
+    tabstop = 4,
+    softtabstop = 4,
+    shiftwidth = 4,
+    shiftround = true,
+    guicursor = "n-v-c-i:block",
+    shellxquote = "",
+    autoindent = true,
+    smartindent = true,
+    scrolloff = 5,
+    equalalways = true,
+    mouse = "",
+    number = true,
+    relativenumber = true,
+    fillchars = { vert = ' ', },
 }
 
 for option, value in pairs(options) do
@@ -86,10 +81,6 @@ end
 vim.cmd ([[ 
     autocmd FileType * set formatoptions-=cro 
 ]])
-
------------------------------
---[[ Lazy (plugin manager) ]]
------------------------------
 
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
